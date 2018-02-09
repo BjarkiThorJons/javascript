c6fa14993f78d6a4bbafdb67213d59e5db6a23eb
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-02-09 10:27:41 +0000

    Add files via upload

---
+++ verkefni_5/quiz.js
@@ -0,0 +1,28 @@
+function quiz(spurning,svarmoguleikar,rettsvar){
+	this.spurning=spurning
+	this.svarmoguleikar=svarmoguleikar
+	this.rettsvar=rettsvar
+};
+
+let quiz1=new quiz("Hver stal kökunni úr krúsinni í gær",["Bjarki","Ágúst","Atli","Hnoðri"],"Ágúst");
+let quiz2=new quiz("Er Atli góður í cs?",["True","False"],"True");
+listi=[quiz1,quiz2]
+tala=Math.round(Math.random())
+function adderino(spurning,svarmoguleikar,rettsvar){
+		svarStrengur="<div>"
+		spurningaStrengur="<div>"+spurning+"</div>"
+		for (x in svarmoguleikar){
+			if (svarmoguleikar[x]==rettsvar){
+				svarStrengur+="<div style='color:green;'>"+svarmoguleikar[x]+"</div>";
+			} else{
+			svarStrengur+="<div>"+svarmoguleikar[x]+"</div>";
+			}
+		}
+		spurningaStrengur+="</div>"
+		strengur=spurningaStrengur+svarStrengur
+		console.log(strengur)
+		document.getElementById("quiz").innerHTML+=strengur
+		
+};
+
+adderino(listi[tala].spurning,listi[tala].svarmoguleikar,listi[tala].rettsvar);
